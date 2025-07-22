9e4bc59eaad55a706c35b84fe5b660b27f567780
Korya25 <????? ??? GitHub>
2025-07-22 14:42:05 +0300

    f

--- lib/core/presentation/app/store_app.dart
+++ lib/core/presentation/app/store_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:store_app/core/network/network_status_wrapper.dart';
 
 class StoreApp extends StatelessWidget {
@@ -6,13 +7,16 @@ class StoreApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Store App',
-      home: NetworkStatusWrapper(
-        child: Scaffold(
-          appBar: AppBar(title: const Text('Store App')),
-          body: const Center(child: Text('🏪 Welcome to Store App')),
+    return ScreenUtilInit(
+      designSize: Size(375, 812),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Store App',
+        home: NetworkStatusWrapper(
+          child: Scaffold(
+            appBar: AppBar(title: const Text('Store App')),
+            body: const Center(child: Text('🏪 Welcome to Store App')),
+          ),
         ),
       ),
     );
