50156c4492dcf3059a51d8b58a6e865d92aea67a
Korya25 <????? ??? GitHub>
2025-07-22 12:57:35 +0300

    add frist check varible

--- lib/core/network/network_status_wrapper.dart
+++ lib/core/network/network_status_wrapper.dart
@@ -2,10 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:store_app/core/network/connectivity_controller.dart';
 import 'package:store_app/core/network/network_banner..dart';
 
-class NetworkStatusWrapper extends StatelessWidget {
+class NetworkStatusWrapper extends StatefulWidget {
   final Widget child;
 
   const NetworkStatusWrapper({super.key, required this.child});
+
+  @override
+  State<NetworkStatusWrapper> createState() => _NetworkStatusWrapperState();
+}
+
+class _NetworkStatusWrapperState extends State<NetworkStatusWrapper> {
+  bool _hasShownInitialBanner = false;
 
   @override
   Widget build(BuildContext context) {
@@ -13,15 +20,22 @@ class NetworkStatusWrapper extends StatelessWidget {
       valueListenable: ConnectivityController.instance.isConnected,
       builder: (context, isConnected, _) {
         WidgetsBinding.instance.addPostFrameCallback((_) {
-          NetworkBanner.show(
-            context,
-            isConnected
-                ? NetworkStatusType.connected
-                : NetworkStatusType.disconnected,
-          );
+          if (!_hasShownInitialBanner) {
+            if (!isConnected) {
+              NetworkBanner.show(context, NetworkStatusType.disconnected);
+            }
+            _hasShownInitialBanner = true;
+          } else {
+            NetworkBanner.show(
+              context,
+              isConnected
+                  ? NetworkStatusType.connected
+                  : NetworkStatusType.disconnected,
+            );
+          }
         });
 
-        return child;
+        return widget.child;
       },
     );
   }
