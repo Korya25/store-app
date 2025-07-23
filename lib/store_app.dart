432fa73fe04590f22dadd7c49e7bbf00881efb42
Korya25 <????? ??? GitHub>
2025-07-23 17:11:49 +0300

    v

--- lib/store_app.dart
+++ lib/store_app.dart
@@ -1,10 +1,9 @@
-// ignore_for_file: depend_on_referenced_packages
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:store_app/core/resources/localization/app_localizations.dart';
 import 'package:store_app/core/resources/style/theme/app_theme.dart';
 import 'package:store_app/core/resources/router/app_router.dart';
+// ignore: depend_on_referenced_packages
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 class StoreApp extends StatelessWidget {
