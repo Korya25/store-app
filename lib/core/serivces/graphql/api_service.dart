cfd9319b1a1503ec4cd06232e68bdcf47708b322
Korya25 <????? ??? GitHub>
2025-07-28 12:59:04 +0300

    add login and user role API methods

--- lib/core/serivces/graphql/api_service.dart
+++ lib/core/serivces/graphql/api_service.dart
@@ -1,12 +1,21 @@
 import 'package:dio/dio.dart';
 import 'package:retrofit/retrofit.dart';
+import 'package:store_app/features/auth/data/models/login_response.dart';
+import 'package:store_app/features/auth/data/models/user_role_response.dart';
 
 part 'api_service.g.dart';
 
 const String baseUrl = 'https://api.escuelajs.co';
 const String graphql = '/graphql';
+const String profile = '/api/v1/auth/profile';
 
 @RestApi(baseUrl: baseUrl)
 abstract class ApiService {
   factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+
+  @POST(graphql)
+  Future<LoginResponse> login(@Body() Map<String, dynamic> mutation);
+
+  @GET(profile)
+  Future<UserRoleResponse> userRole(@Body() Map<String, dynamic> getUserRole);
 }
